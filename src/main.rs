ceb2e021dbc25a38bb20df501304992378d90770
araxnoid <araxnoid0@gmail.com>
2026-03-09 05:04:45 +0700

    created resize adn render method

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::sync::Arc;
 
 use pollster::FutureExt;
 use wgpu::{
-    Backends, Device, DeviceDescriptor, ExperimentalFeatures, Features, Instance,
-    InstanceDescriptor, Limits, Queue, RequestAdapterOptionsBase, Surface, SurfaceConfiguration,
+    Backends, CommandEncoderDescriptor, Device, DeviceDescriptor, ExperimentalFeatures, Features,
+    Instance, InstanceDescriptor, Limits, Operations, Queue, RenderPassColorAttachment,
+    RenderPassDescriptor, RequestAdapterOptionsBase, Surface, SurfaceConfiguration, SurfaceError,
     TextureUsages,
 };
 use winit::{
@@ -27,6 +28,86 @@ struct MyApp {
     core: Option<CoreMyApp>,
 }
 
+impl MyApp {
+    fn ready_to_render(&self) -> bool {
+        !self.core.is_none() && !self.window.is_none()
+    }
+
+    fn render(&self) -> Result<(), SurfaceError> {
+        if !self.ready_to_render() {
+            return Ok(());
+        }
+
+        self.window.as_ref().unwrap().request_redraw();
+
+        if !self.core.as_ref().unwrap().is_surface_cfg {
+            return Ok(());
+        }
+
+        let output = self.core.as_ref().unwrap().surface.get_current_texture()?;
+
+        let view = output
+            .texture
+            .create_view(&wgpu::wgt::TextureViewDescriptor {
+                ..Default::default()
+            });
+
+        let mut encoder =
+            self.core
+                .as_ref()
+                .unwrap()
+                .device
+                .create_command_encoder(&CommandEncoderDescriptor {
+                    label: Some("Create Encoder"),
+                });
+
+        {
+            let brp = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: Some("Create Render Pass"),
+                depth_stencil_attachment: None,
+                multiview_mask: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+                color_attachments: &[Some(RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    depth_slice: None,
+                    ops: Operations {
+                        store: wgpu::StoreOp::Store,
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                    },
+                })],
+            });
+        }
+
+        self.core
+            .as_ref()
+            .unwrap()
+            .queue
+            .submit(Some(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+
+    fn resize(&mut self, width: u32, height: u32) {
+        if !self.core.is_none() {
+            let core = self.core.as_mut().unwrap();
+            if width > 0 && height > 0 {
+                core.config.height = height;
+                core.config.width = width;
+                core.surface.configure(&core.device, &core.config);
+                core.is_surface_cfg = true;
+            }
+        }
+    }
+}
+
 impl ApplicationHandler for MyApp {
     fn window_event(
         &mut self,
@@ -36,14 +117,32 @@ impl ApplicationHandler for MyApp {
     ) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => self.resize(size.width, size.height),
+            WindowEvent::RedrawRequested => match self.render() {
+                Ok(_) => {}
+                Err(SurfaceError::Lost | SurfaceError::Outdated) => {
+                    let inner_size = self.window.as_ref().unwrap().inner_size();
+                    self.resize(inner_size.width, inner_size.height);
+                }
+                _ => (),
+            },
             _ => (),
         }
     }
 
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        if self.core.is_none() {
-            // init
+        if self.window.is_none() {
             let inner_size = (800, 500);
+            let attribute = Window::default_attributes()
+                .with_title("my app")
+                .with_inner_size(LogicalSize::new(inner_size.0, inner_size.1));
+
+            let window = event_loop
+                .create_window(attribute)
+                .expect("Error Create Window");
+            self.window = Some(Arc::new(window));
+            println!("window done");
+
             // wgpu
             let instance = Instance::new(&InstanceDescriptor {
                 backends: Backends::all(),
@@ -97,14 +196,6 @@ impl ApplicationHandler for MyApp {
             };
             // wgpu
 
-            let attribute = Window::default_attributes()
-                .with_title("my app")
-                .with_inner_size(LogicalSize::new(inner_size.0, inner_size.1));
-
-            let window = event_loop
-                .create_window(attribute)
-                .expect("Error Create Window");
-
             let core = CoreMyApp {
                 config,
                 device,
@@ -113,7 +204,7 @@ impl ApplicationHandler for MyApp {
                 is_surface_cfg: false,
             };
             self.core = Some(core);
-            self.window = Some(Arc::new(window));
+            println!("wgpu done");
         }
     }
 }
